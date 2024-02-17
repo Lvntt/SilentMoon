29d48a43d7ed726720cd10cf06e50dde2a46cfeb
Matvey Seregin <daymatt@mail.ru>
2024-02-17 22:32:32 +0700

    add sign up screen ui

--- src/main/java/dev/lantt/silentmoon/presentation/SignUpFragment.kt
+++ src/main/java/dev/lantt/silentmoon/presentation/SignUpFragment.kt
@@ -1,20 +1,58 @@
 package dev.lantt.silentmoon.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dev.lantt.silentmoon.R
+import dev.lantt.silentmoon.databinding.FragmentSignUpBinding
+import dev.lantt.silentmoon.utils.makeSpannableString
 
 class SignUpFragment : Fragment() {
+
+    // TODO change
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        val spannableString = makeSpannableString(
+            text = resources.getString(R.string.iHaveReadThePrivacyPolicy),
+            phrase = resources.getString(R.string.privacyPolicy),
+            phraseColor = ContextCompat.getColor(requireContext().applicationContext,
+                R.color.accent
+            )
+        ) {
+            Toast.makeText(
+                context,
+                resources.getString(R.string.privacyPolicyText),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        with (binding.noAccount) {
+            movementMethod = LinkMovementMethod.getInstance()
+            text = spannableString
+        }
+        binding.getStartedButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_welcomeFragment)
+        }
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
