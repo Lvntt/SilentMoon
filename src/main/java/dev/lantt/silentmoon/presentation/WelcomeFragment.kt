7b7e9b1e882020b23a7825f377960ae8cd6b151f
Matvey Seregin <daymatt@mail.ru>
2024-02-18 16:15:30 +0700

    add welcome screen ui
    - added welcome screen ui
    - added passing username between fragments using bundle

--- src/main/java/dev/lantt/silentmoon/presentation/WelcomeFragment.kt
+++ src/main/java/dev/lantt/silentmoon/presentation/WelcomeFragment.kt
@@ -1,20 +1,51 @@
 package dev.lantt.silentmoon.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import dev.lantt.silentmoon.R
+import dev.lantt.silentmoon.databinding.FragmentWelcomeBinding
+import dev.lantt.silentmoon.utils.setTopMarginInset
 
 class WelcomeFragment : Fragment() {
+
+    // TODO change
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+
+        _binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
+
+        setTopMarginInset(binding.appTitle)
+
+        var username = arguments?.getString("username")
+        if (username.isNullOrBlank()) {
+            username = "Afsar"
+        }
+        val spannableString = SpannableString(resources.getString(R.string.welcomeGreetingsText, username))
+        val boldString = resources.getString(R.string.welcomeGreetingsTextShort, username)
+        val start = 0
+        val end = start + boldString.length
+
+        spannableString.setSpan(
+            TextAppearanceSpan(context, R.style.welcomeBoldTitleText),
+            start,
+            end,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.greetingsText.text = spannableString
+
+        return binding.root
     }
 
 }
