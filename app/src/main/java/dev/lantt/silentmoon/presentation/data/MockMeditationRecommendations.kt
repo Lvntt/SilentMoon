c13283d17687914990f1f645b8eb8357bcf81380
Matvey Seregin <daymatt@mail.ru>
2024-02-27 17:25:12 +0700

    change app git folder to root

---
+++ app/src/main/java/dev/lantt/silentmoon/presentation/data/MockMeditationRecommendations.kt
@@ -0,0 +1,42 @@
+package dev.lantt.silentmoon.presentation.data
+
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import dev.lantt.silentmoon.R
+
+object MockMeditationRecommendations {
+    val recommendations = listOf(
+        MeditationRecommendation(
+            title = R.string.meditationRecommendationFocus,
+            description = R.string.meditationRecommendationDescription,
+            image = R.drawable.img_focus_recommendation,
+            backgroundColor = R.color.meditationRecommendationFocusBgColor
+        ),
+        MeditationRecommendation(
+            title = R.string.meditationRecommendationHappiness,
+            description = R.string.meditationRecommendationDescription,
+            image = R.drawable.img_happiness_recommendation,
+            backgroundColor = R.color.meditationRecommendationHappinessBgColor
+        ),
+        MeditationRecommendation(
+            title = R.string.meditationRecommendationFocus,
+            description = R.string.meditationRecommendationDescription,
+            image = R.drawable.img_focus_recommendation,
+            backgroundColor = R.color.meditationRecommendationFocusBgColor
+        ),
+        MeditationRecommendation(
+            title = R.string.meditationRecommendationHappiness,
+            description = R.string.meditationRecommendationDescription,
+            image = R.drawable.img_happiness_recommendation,
+            backgroundColor = R.color.meditationRecommendationHappinessBgColor
+        ),
+    )
+}
+
+data class MeditationRecommendation(
+    @StringRes val title: Int,
+    @StringRes val description: Int,
+    @DrawableRes val image: Int,
+    @ColorRes val backgroundColor: Int
+)
