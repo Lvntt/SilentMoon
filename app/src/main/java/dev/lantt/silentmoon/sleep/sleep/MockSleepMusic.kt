e3af0b744d8107aabb7ebca419d86305ea29d5ff
Matvey Seregin <daymatt@mail.ru>
2024-03-05 19:34:45 +0700

    add play option screen

--- app/src/main/java/dev/lantt/silentmoon/sleep/sleep/MockSleepMusic.kt
+++ app/src/main/java/dev/lantt/silentmoon/sleep/sleep/MockSleepMusic.kt
@@ -22,6 +22,22 @@ object MockSleepMusic {
             image = R.drawable.img_moon_clouds,
             title = R.string.storyMoonClouds
         ),
+        SleepMusic(
+            image = R.drawable.img_night_island,
+            title = R.string.storyNightIsland
+        ),
+        SleepMusic(
+            image = R.drawable.img_sweet_sleep,
+            title = R.string.storySweetSleep
+        ),
+        SleepMusic(
+            image = R.drawable.img_good_night,
+            title = R.string.storyGoodNight
+        ),
+        SleepMusic(
+            image = R.drawable.img_moon_clouds,
+            title = R.string.storyMoonClouds
+        ),
     )
 }
 
